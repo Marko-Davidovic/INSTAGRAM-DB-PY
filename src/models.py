6f613ed0eec50c3605bcdad6e10691cdb6a3d44b
Marko Davidovic <davidovic-m@hotmail.com>
2020-02-19 23:08:05 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -29,5 +29,38 @@ class Address(Base):
     def to_dict(self):
         return {}
 
+class home(Base):
+    __tablename__ = 'Home'
+    id = Column(Integer, primary_key=True)
+    story = Column(String(250))
+    profile = Column(String(250))
+    like = Column(String(250))
+    picture = Column(String(250), nullable=False)
+    home_id = Column(Integer, ForeignKey('home.id'))
+    home = relationship(home)
+
+class story(Base):
+    __tablename__ = 'story'
+    id = Column(Integer, primary_key=True)
+    live = Column(String(250))
+    boomerang = Column(String(250))
+    superzoom = Column(String(250))
+    Filter = Column(String(200))
+    story_id = Column(Integer, ForeignKey('story.id'))
+    story = relationship(story)
+
+class profile(Base):
+    __tablename__ = 'profile'
+    id = Column(Integer, primary_key=True)
+    posts = Column(String(250))
+    fallowers = Column(String(250))
+    following = Column(String(250))
+    messages = Column(String(250))
+    tagged_picture = Column(String(250), nullable=False)
+    profile_id = Column(Integer, ForeignKey('profile.id'))
+    profile = relationship(profile)
+
+def to_dict(self):
+        return {}
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
